ee6ed66884ccf90f8ff8da7c701896102e924f45
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-18 21:57:26 +0000

    implemented counter, currently works for keypress, not working if you copy paste text in yet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,17 @@
+const updateCounter = (counterPassed) => {
+  $( 'span.counter ').text(140 - counterPassed);
+}
+
 $(document).ready(function() {
-  
-  $( '.new-tweet textarea' ).on('input', function() {
-    
+  let charCounter = 0;
+  $( '.new-tweet textarea' ).on('keypress', function() {
+    charCounter++ ;
+    updateCounter(charCounter);
+    if (charCounter > 140){
+      $( ' span.counter ').addClass(' over-limit ');
+    } else {
+      $(' span.counter ').removeClass(' over-limit ');
+    }
   })
 });
-
 
