7791ce84a6cb903efb3f977d43a64ba367d024e5
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-19 01:25:34 +0000

    created the tweets, added hover effect over the tweet to show the user handle, all tweet data renders correctly on page. Only styling needed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,55 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
 const createTweetElement = function(tweet) {
+  // $( '.all-tweets' )
   let tweetArticle = $( '<article>' );
   let tweetHeader = $( '<header>' );
   let tweetFooter = $( '<footer>' );
-  tweetHeader.append($( '<img src="/images/profile-hex.png">' ));
-  tweetHeader.append($( '<span>' ).text( 'name' ));
-  tweetHeader.append($( '<span>' ).text( '@userhandle' ));
+  tweetHeader.append($( '<img>' ).attr("src", tweet.user.avatars));
+  tweetHeader.append($( '<span>' ).text( tweet.user.name ));
+  tweetHeader.append($( '<span>' ).addClass( 'hover-class' ).text( tweet.user.handle ));
   tweetArticle.append($(tweetHeader));
-  tweetArticle.append($( '<div>' ).text( 'this is my tweet. boom.' ));
+  tweetArticle.append($( '<div>' ).text( tweet.content.text ));
   tweetFooter.append($( '<div>' ));
+  console.log(tweetArticle[0]);
+  return tweetArticle;
 }
 
+const renderTweets = function(tweets) {
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
+  const tweetsContainer = $( '.all-tweets' );
+  console.log(tweets);
+  console.log(tweetsContainer[0])
+  tweetsContainer.append(createTweetElement(tweets[0]));
 }
 
-const $tweet = createTweetElement(tweetData);
+$(document).ready(() => {
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  renderTweets(data);
+});
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
