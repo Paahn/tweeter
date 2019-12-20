b470c3521e4a31c6a16c60c904c2ec785bb3f091
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-20 00:38:04 +0000

    finally loading all tweets, prepending the new ones to the top

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,38 +6,58 @@
 
 
 const createTweetElement = function(tweet) {
-  // $( '.all-tweets' )
-  let tweetArticle = $( '<article>' );
-  let tweetHeader = $( '<header>' );
-  let tweetFooter = $( '<footer>' );
-  tweetHeader.append($( '<img>' ).attr("src", tweet.user.avatars));
-  tweetHeader.append($( '<span>' ).text( tweet.user.name ));
-  tweetHeader.append($( '<span>' ).addClass( 'hover-class' ).text( tweet.user.handle ));
+  let tweetArticle = $('<article>');
+  let tweetHeader = $('<header>');
+  let tweetFooter = $('<footer>');
+  tweetHeader.append($('<img>').attr("src", tweet.user.avatars));
+  tweetHeader.append($('<span>').text(tweet.user.name));
+  tweetHeader.append($('<span>').addClass('hover-class').text(tweet.user.handle));
   tweetArticle.append($(tweetHeader));
-  tweetArticle.append($( '<div>' ).text( tweet.content.text ));
-  tweetFooter.append($( '<div>' ));
-  console.log(tweetArticle[0]);
+  tweetArticle.append($('<div>').text(tweet.content.text));
+  tweetFooter.append($('<div>'));
+
   return tweetArticle;
-}
+};
 
-const renderTweets = function(tweets) {
 
-  const tweetsContainer = $( '.all-tweets' );
-  console.log(tweets);
-  console.log(tweetsContainer[0])
-  tweetsContainer.append(createTweetElement(tweets[0]));
-}
+const renderTweets = function(tweet) {
+  tweet.forEach((tweet) => {
+    const tweetsContainer = $('.all-tweets');
+    tweetsContainer.prepend(createTweetElement(tweet));
+  });
+  
+};
+
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+    success: function(tweetyTweets) {
+      renderTweets(tweetyTweets);
+    }
+  });
+};
+
 
 $(document).ready(() => {
-  $('.form-class').submit((event) =>  {
+  $('.form-class').submit((event) => {
     event.preventDefault();
+
     let text = $('#new-tweet-textarea').val();
-    console.log(text);
+    // console.log(text);
+    // let variableA = $('form').serialize();
+    // console.log(variableA);
+
+    $.ajax({
+      url: '/tweets/',
+      method: 'POST',
+      data: $('#new-tweet-textarea').serialize()
+    })
+      .then(() => {
+        loadTweets();
+      });
+
   });
-
-
-
-
 
   const data = [
     {
@@ -56,13 +76,15 @@ $(document).ready(() => {
       "user": {
         "name": "Descartes",
         "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
+        "handle": "@rd"
+      },
       "content": {
         "text": "Je pense , donc je suis"
       },
       "created_at": 1461113959088
     }
   ];
-  renderTweets(data);
+  loadTweets();
+  // renderTweets(data);
 });
 
