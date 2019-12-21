d81aef18181362c5ab5725a5df18607d95fa3ed1
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-21 02:48:50 +0000

    added datestamp to footer

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,11 +14,39 @@ const createTweetElement = function(tweet) {
   tweetHeader.append($('<span>').text(tweet.user.name));
   tweetHeader.append($('<span>').addClass('hover-class').text(tweet.user.handle));
   tweetArticle.append($(tweetHeader));
-  tweetArticle.append($('<div>').text(tweet.content.text));
-  tweetFooter.append($('<div>'));
+  tweetArticle.append($('<p>').addClass('stay-inside-text').text(tweet.content.text));
+  tweetFooter.append($('<div>').addClass('time'));
+  tweetFooter.append($('<span>')).text(`${timeSinceTweeted(new Date(), new Date(tweet.created_at))}`);
+  tweetArticle.append($(tweetFooter));
 
   return tweetArticle;
 };
+
+const timeSinceTweeted = function(current, previous) {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  const elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return Math.round(elapsed / 1000) + " seconds ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return "about " + Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return "about " + Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    return "about " + Math.round(elapsed / msPerYear) + " years ago";
+  }
+};
+
+
 
 // shows tweets on app page
 const renderTweets = function(tweet) {
